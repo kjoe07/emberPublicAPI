29f92ffb2c279c94dc2a4fc503b1852cd747fd1a
Yoel Jimenez del Valle <yoel07@gmail.com>
2019-07-15 10:54:58 -0400

    Initial Commit

---
+++ emberPublicAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emberPublicAPI
+//
+//  Created by Yoel Jimenez del Valle on 7/15/19.
+//  Copyright © 2019 yoel jimenez del valle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
